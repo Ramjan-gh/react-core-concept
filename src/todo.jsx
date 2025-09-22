3a163109ac14009625a2c4ad9e9213db815a929d
ramjan-gh <ramzanhridoy@gmail.com>
2025-09-22 21:10:39 +0600

    third

--- src/todo.jsx
+++ src/todo.jsx
@@ -4,15 +4,27 @@
 //     )
 // }
 
+// export default function ToDo({task, isDone, time=0}){
+//         if(isDone===true){
+//             return(
+//                 <li>Done: {task} Duration: {time}</li>
+//             )
+//         }
+//         else{
+//             return(
+//                 <li>Do Now: {task}</li>
+//             )
+//         }
+// }
+
 export default function ToDo({task, isDone, time=0}){
-        if(isDone===true){
-            return(
-                <li>Done: {task} Duration: {time}</li>
-            )
-        }
-        else{
-            return(
-                <li>Do Now: {task}</li>
-            )
-        }
+    return isDone ? (
+      <li>
+        Done: {task} Time: {time}
+      </li>
+    ) : (
+      <li>
+        Not Done: {task} Time: {time}
+      </li>
+    );
 }
